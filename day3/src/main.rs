af8c77df7f2f238e4a24ad17a2024996be63edca
Alexandre Fourcat <afourcat@gmail.com>
2021-12-03 17:05:39 +0100

    :sparkles: Add part2.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,42 +1,36 @@
 use utils::get_input;
 
 fn main() {
-    let values = get_input::<String>();
-    let bin_length = values[0].len();
-    let gamma_str = compute_gamma_str(&values, bin_length);
+    let strings: Vec<String> = get_input();
 
-    let (gamma, epsilon) = get_epsilon_gamma_as_number(gamma_str, bin_length);
-    println!(
-        "Gamma: {:#08b}\nEpsilon: {:#08b}\noutput: {}",
-        gamma,
-        epsilon,
-        gamma * epsilon
-    );
+    let oxygen = get(strings.clone(), 0, |acc| acc >= 0);
+    println!("Oxygen: {:?}", oxygen);
+    let co2 = get(strings, 0, |acc| acc < 0);
+    println!("CO2: {:?}", co2);
+    println!("Output: {}", co2 * oxygen);
 }
 
-fn get_epsilon_gamma_as_number(gamma_str: String, bin_length: usize) -> (u32, u32) {
-    let gamma = u32::from_str_radix(&gamma_str, 2).unwrap();
-    let offset = 32 - bin_length;
-    let mask = (u32::MAX << offset) >> offset;
-    (gamma, mask ^ gamma)
-}
+fn get<P: Fn(i32) -> bool>(strings: Vec<String>, idx: usize, predicate: P) -> u32 {
+    let acc = get_most_common(&strings, idx);
+    let char = if predicate(acc) { '1' } else { '0' };
+    let output = strings
+        .into_iter()
+        .filter(|string| string.as_bytes()[idx] == char as u8)
+        .collect::<Vec<String>>();
 
-fn compute_gamma_str(values: &[String], bin_length: usize) -> String {
-    let mut gamma_str = String::with_capacity(bin_length);
-
-    for i in 0..bin_length {
-        let mut acc = 0;
-
-        for j in 0..values.len() {
-            if values[j].as_bytes()[i] == '1' as u8 {
-                acc += 1;
-            } else {
-                acc -= 1;
-            }
-        }
-
-        let char = if acc > 0 { '1' } else { '0' };
-        gamma_str.push(char);
+    if output.len() == 1 {
+        u32::from_str_radix(&output[0], 2).unwrap()
+    } else {
+        get(output, idx + 1, predicate)
     }
-    gamma_str
+}
+
+fn get_most_common(strings: &[String], idx: usize) -> i32 {
+    let mut acc = 0;
+
+    for string in strings {
+        let char = string.as_bytes()[idx];
+        acc += if char == '1' as u8 { 1 } else { -1 }
+    }
+    acc
 }
