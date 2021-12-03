db35a4e46202834a52964badcb0481c8f2a155a4
Alexandre Fourcat <afourcat@gmail.com>
2021-12-03 17:19:56 +0100

    :art: Clean code.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,15 +3,16 @@ use utils::get_input;
 fn main() {
     let strings: Vec<String> = get_input();
 
-    let oxygen = get(strings.clone(), 0, |acc| acc >= 0);
+    let oxygen = compute_value(strings.clone(), 0, |acc| acc >= 0);
     println!("Oxygen: {:?}", oxygen);
-    let co2 = get(strings, 0, |acc| acc < 0);
+    let co2 = compute_value(strings, 0, |acc| acc < 0);
     println!("CO2: {:?}", co2);
+
     println!("Output: {}", co2 * oxygen);
 }
 
-fn get<P: Fn(i32) -> bool>(strings: Vec<String>, idx: usize, predicate: P) -> u32 {
-    let acc = get_most_common(&strings, idx);
+fn compute_value<P: Fn(i32) -> bool>(strings: Vec<String>, idx: usize, predicate: P) -> u32 {
+    let acc = bitcmp(&strings, idx);
     let char = if predicate(acc) { '1' } else { '0' };
     let output = strings
         .into_iter()
@@ -21,11 +22,11 @@ fn get<P: Fn(i32) -> bool>(strings: Vec<String>, idx: usize, predicate: P) -> u3
     if output.len() == 1 {
         u32::from_str_radix(&output[0], 2).unwrap()
     } else {
-        get(output, idx + 1, predicate)
+        compute_value(output, idx + 1, predicate)
     }
 }
 
-fn get_most_common(strings: &[String], idx: usize) -> i32 {
+fn bitcmp(strings: &[String], idx: usize) -> i32 {
     let mut acc = 0;
 
     for string in strings {
