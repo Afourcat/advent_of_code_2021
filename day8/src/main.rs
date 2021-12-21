71de40c270b2f034d359a472ddd77ea66fb3f43a
Alexandre Fourcat <afourcat@gmail.com>
2021-12-21 16:35:00 +0100

    :sparkles: Push **horrible**, but working, day8 part2.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code)]
-use std::str::FromStr;
+use std::{arch::x86_64::_mm_andnot_ps, collections::HashMap, num, ops::Deref, str::FromStr};
 
 use utils::get_input;
 
@@ -16,6 +16,13 @@ impl Digit {
         Self {
             inner: from.as_ref().to_string(),
         }
+    }
+
+    fn translate(&self, mapping: &HashMap<char, char>) -> String {
+        self.inner
+            .chars()
+            .map(|letter| mapping.get(&letter).unwrap())
+            .collect::<String>()
     }
 }
 
@@ -41,6 +48,7 @@ impl FromStr for Line {
 }
 
 impl Line {
+    // Compute the number of occurences of digits: 1, 4, 7, 8.
     fn part1(&self) -> u32 {
         let mut occurences: [u32; 10] = [0; 10];
 
@@ -57,10 +65,201 @@ impl Line {
         let total = occurences[1] + occurences[4] + occurences[7] + occurences[8];
         total
     }
+
+    fn solve_mapping(&self) -> HashMap<&str, u8> {
+        let mut mapping: HashMap<char, char> = HashMap::with_capacity(7);
+        let mut number_mapping: HashMap<&str, u8> = HashMap::with_capacity(9);
+
+        let one = &self
+            .digits
+            .iter()
+            .find(|e| e.inner.len() == 2)
+            .unwrap()
+            .inner;
+        number_mapping.insert(&one, 1);
+
+        let four = &self
+            .digits
+            .iter()
+            .find(|e| e.inner.len() == 4)
+            .unwrap()
+            .inner;
+
+        number_mapping.insert(&four, 4);
+        let seven = &self
+            .digits
+            .iter()
+            .find(|e| e.inner.len() == 3)
+            .unwrap()
+            .inner;
+
+        number_mapping.insert(&seven, 7);
+        let height = &self
+            .digits
+            .iter()
+            .find(|e| e.inner.len() == 7)
+            .unwrap()
+            .inner;
+        number_mapping.insert(&height, 8);
+
+        Self::solve_a(&mut mapping, one.as_bytes(), seven.as_bytes());
+        self.solve_3(&mapping, &mut number_mapping, one.as_bytes());
+        self.solve_5(&mut number_mapping, four.as_bytes());
+        self.solve_2(&mut number_mapping);
+        self.solve_9(&mut number_mapping, four.as_bytes());
+        self.solve_0(&mut number_mapping, one.as_bytes());
+        self.solve_6(&mut number_mapping);
+
+        number_mapping
+    }
+
+    fn solve_a(mapping: &mut HashMap<char, char>, one: &[u8], seven: &[u8]) {
+        let rest = seven
+            .iter()
+            .cloned()
+            .filter(|letter| !one.contains(letter))
+            .next()
+            .unwrap();
+
+        mapping.insert('a', rest as char);
+    }
+
+    // Deduce the wiring and compute the output value.
+    fn part2(&self) -> u32 {
+        let mapping = self.solve_mapping();
+        println!("Mapping {:?}", mapping);
+        let mut buf: [u8; 4] = [0; 4];
+
+        for (idx, digit) in self.outputs.iter().enumerate() {
+            let mut sorted_digit = digit.inner.clone().into_bytes();
+            sorted_digit.sort();
+
+            let (key, value) = mapping
+                .iter()
+                .find(|(key, value)| {
+                    let mut cloned_key = key.deref().deref().to_owned().into_bytes();
+                    cloned_key.sort();
+                    cloned_key == sorted_digit
+                })
+                .unwrap();
+            buf[idx] = (*value) + 48;
+        }
+        let str_output = std::str::from_utf8(&buf).unwrap();
+        println!("{}", str_output);
+        str_output.parse().unwrap()
+    }
+
+    fn solve_6<'a>(&'a self, number_mapping: &mut HashMap<&'a str, u8>) {
+        let digit = self
+            .digits
+            .iter()
+            .find(|digit| !number_mapping.contains_key(digit.inner.as_str()));
+
+        println!("Map 6 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 6);
+    }
+
+    fn solve_0<'a>(&'a self, number_mapping: &mut HashMap<&'a str, u8>, one: &[u8]) {
+        let digit = self
+            .digits
+            .iter()
+            .filter(|digit| digit.inner.len() == 6)
+            .find(|digit| {
+                let is_not_9 = number_mapping
+                    .get(digit.inner.as_str())
+                    .map_or(true, |value| *value != 9);
+
+                let count = digit
+                    .inner
+                    .as_bytes()
+                    .iter()
+                    .filter(|digit| one.contains(digit))
+                    .count();
+                count == 2 && is_not_9
+            });
+
+        println!("Map 0 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 0);
+    }
+
+    fn solve_9<'a>(&'a self, number_mapping: &mut HashMap<&'a str, u8>, four: &[u8]) {
+        let digit = self
+            .digits
+            .iter()
+            .filter(|digit| digit.inner.len() == 6)
+            .find(|digit| {
+                digit
+                    .inner
+                    .as_bytes()
+                    .iter()
+                    .filter(|digit| four.contains(digit))
+                    .count()
+                    == 4
+            });
+
+        println!("Map 9 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 9);
+    }
+
+    fn solve_2<'a>(&'a self, number_mapping: &mut HashMap<&'a str, u8>) {
+        let digit = self
+            .digits
+            .iter()
+            .filter(|digit| digit.inner.len() == 5)
+            .find(|digit| !number_mapping.contains_key(digit.inner.as_str()));
+
+        println!("Map 2 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 2);
+    }
+
+    fn solve_5<'a>(&'a self, number_mapping: &mut HashMap<&'a str, u8>, four: &[u8]) {
+        let digit = self
+            .digits
+            .iter()
+            .filter(|digit| digit.inner.len() == 5)
+            .find(|digit| {
+                let count = digit
+                    .inner
+                    .as_bytes()
+                    .iter()
+                    .filter(|c| four.contains(c))
+                    .count();
+                !number_mapping.contains_key(digit.inner.as_str()) && count == 3
+            });
+
+        println!("Map 5 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 5);
+    }
+
+    fn solve_3<'a>(
+        &'a self,
+        mapping: &HashMap<char, char>,
+        number_mapping: &mut HashMap<&'a str, u8>,
+        one: &[u8],
+    ) {
+        let digit = self
+            .digits
+            .iter()
+            .filter(|digit| digit.inner.len() == 5)
+            .find(|digit| {
+                let count = digit
+                    .inner
+                    .as_bytes()
+                    .iter()
+                    .filter(|c| one.contains(c))
+                    .count();
+                let a = mapping.get(&'a').unwrap();
+                let contains_a_seg = digit.inner.as_bytes().contains(&(*a as u8));
+                count == 2 && contains_a_seg
+            });
+
+        println!("Map 3 to {}", digit.unwrap().inner);
+        number_mapping.insert(&digit.unwrap().inner, 3);
+    }
 }
 
 fn main() {
     let lines = get_input::<Line>();
-    let total = lines.into_iter().map(|line| line.part1()).sum::<u32>();
+    let total = lines.into_iter().map(|line| line.part2()).sum::<u32>();
     println!("Occurences Total: {}", total);
 }
